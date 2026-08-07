48c98f5d9f24dda8111955083f05172868642507
NewBoringMan <160587798+NewBoringMan@users.noreply.github.com>
2026-08-07 18:23:38 +0800

    Use Taipei timestamps in check-in logs

--- logging_config.py
+++ logging_config.py
@@ -1,13 +1,12 @@
-import logging.config
 import datetime
+import logging
+import logging.config
 
 
-def beijing_time_converter(timestamp):
-    """将时间转换为北京时间"""
+def taipei_time_converter(timestamp):
     utc_dt = datetime.datetime.fromtimestamp(timestamp, tz=datetime.timezone.utc)
-    beijing_tz = datetime.timezone(datetime.timedelta(hours=8))
-    beijing_dt = utc_dt.astimezone(beijing_tz)
-    return beijing_dt.timetuple()
+    taipei = datetime.timezone(datetime.timedelta(hours=8))
+    return utc_dt.astimezone(taipei).timetuple()
 
 
 LOGGING_CONFIG = {
@@ -17,24 +16,16 @@ LOGGING_CONFIG = {
         "standard": {
             "format": "%(asctime)s - %(levelname)s - %(message)s",
             "datefmt": "%Y-%m-%d %H:%M:%S",
-            "converter": beijing_time_converter,
-        },
+            "converter": taipei_time_converter,
+        }
     },
     "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "standard",
-            "level": "INFO",
-        },
+        "console": {"class": "logging.StreamHandler", "formatter": "standard", "level": "INFO"}
     },
-    "root": {
-        "handlers": ["console"],
-        "level": "INFO",
-    },
+    "root": {"handlers": ["console"], "level": "INFO"},
 }
 
 
 def init_logger():
-    """初始化日志"""
     logging.config.dictConfig(LOGGING_CONFIG)
     return logging.getLogger()
